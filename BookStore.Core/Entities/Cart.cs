d6de6a7cc36e8ffe34e8a94226c1bdfe3240a2da
Hasnaa <a02040456@gmail.com>
2026-03-10 16:13:13 +0200

    fix auto-generated id

--- BookStore.Core/Entities/Cart.cs
+++ BookStore.Core/Entities/Cart.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace BookStore.Core.Entities
 {
     public class Cart
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
     }
 }
--- BookStore.Core/Entities/Category.cs
+++ BookStore.Core/Entities/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ namespace BookStore.Core.Entities
 {
     public class Category
     {
+        [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public string Name { get; set; }
