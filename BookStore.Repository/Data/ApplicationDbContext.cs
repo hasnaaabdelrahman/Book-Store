c240f396e857fa4d761e67b611d7fe0e0947e2eb
Hasnaa <a02040456@gmail.com>
2026-03-10 16:42:37 +0200

    Dbcontext

--- BookStore.Repository/Data/ApplicationDbContext.cs
+++ BookStore.Repository/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace BookStore.Repository.Data
         {
                 
         }
+       
 
         public DbSet<Book> books { get; set; }
         public DbSet<Category> categories { get; set; }
