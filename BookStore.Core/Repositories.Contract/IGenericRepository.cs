73d59851337b6a59811edaaefbbd9a26ef77607f
Hasnaa <a02040456@gmail.com>
2026-03-10 03:25:23 +0200

    Implement generic repository

---
+++ BookStore.Core/Repositories.Contract/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Repositories.Contract
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        public Task<IReadOnlyList<T>> GetAllAsync();
+        public Task<T> GetByIdAsync(Guid id);
+        public void AddAsync(T entity);
+        public void UpdateAsync(T entity);
+        public void DeleteAsync(T entity);
+
+
+    }
+}
---
+++ BookStore.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using BookStore.Core.Repositories.Contract;
+using BookStore.Repository.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Repository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async void AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+        }
+
+        public void DeleteAsync(T entity)
+        {
+           _context.Set<T>().Remove(entity);
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+             return await _context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public void UpdateAsync(T entity)
+        {
+           _context.Set<T>().Update(entity);
+        }
+    }
+}
