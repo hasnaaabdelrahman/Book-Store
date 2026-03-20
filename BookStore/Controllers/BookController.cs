495b5fa208c338fb521ab2c2aa64925208d41617
Hasnaa <a02040456@gmail.com>
2026-03-21 00:34:19 +0200

    book controller

---
+++ BookStore/Controllers/BookController.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Core.Services.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookServices;
+        public BookController(IBookService bookService)
+        {
+            _bookServices = bookService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Book>>> GetAllBooks()
+        {
+            var books = await _bookServices.GetAllBooksAsync();
+            return Ok(books);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Book>> GetBookById(Guid id)
+        {
+            var book = await _bookServices.GetBookByIdAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+    }
+}
