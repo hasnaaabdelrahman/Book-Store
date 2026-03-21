ecc9bb9874cc3fac866d10ac10dafa7247edf90b
Hasnaa <a02040456@gmail.com>
2026-03-21 22:05:21 +0200

    Dtos

---
+++ BookStore/Dtos/Incoming/CreateBookDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStore.Dtos.Incoming
+{
+    public class CreateBookDto
+    {
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public Guid? CategoryId { get; set; }
+    }
+}
---
+++ BookStore/Dtos/Incoming/UpdateBookDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookStore.Dtos.Incoming
+{
+    public class UpdateBookDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
