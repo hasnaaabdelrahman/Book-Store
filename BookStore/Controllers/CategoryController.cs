9b0a22cb6e5c1156dd4787042a9d7192724a7457
Hasnaa <a02040456@gmail.com>
2026-03-23 01:18:16 +0200

    Category Controller

---
+++ BookStore/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using BookStore.Core.Entities;
+using BookStore.Core.Services.Contract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<Category>>> GetAll()
+        {
+           var categories = await _categoryService.GetAllCategoriesAsync();
+              return Ok(categories);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Category>>GetById(Guid id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return Ok(category);
+        }
+    }
+}
