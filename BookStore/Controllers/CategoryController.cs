d51f764560a91887d70a3c47c36c798da82d93bd
Hasnaa <a02040456@gmail.com>
2026-03-25 00:20:27 +0200

    update && insert && delete - category apis

--- BookStore/Controllers/CategoryController.cs
+++ BookStore/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using BookStore.Core.Entities;
 using BookStore.Core.Services.Contract;
+using BookStore.Dtos.Incoming;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -18,12 +19,12 @@ namespace BookStore.Controllers
         [HttpGet]
         public async Task<ActionResult<IReadOnlyList<Category>>> GetAll()
         {
-           var categories = await _categoryService.GetAllCategoriesAsync();
-              return Ok(categories);
+            var categories = await _categoryService.GetAllCategoriesAsync();
+            return Ok(categories);
         }
         [HttpGet]
         [Route("{id}")]
-        public async Task<ActionResult<Category>>GetById(Guid id)
+        public async Task<ActionResult<Category>> GetById(Guid id)
         {
             var category = await _categoryService.GetCategoryByIdAsync(id);
             if (category == null)
@@ -31,6 +32,42 @@ namespace BookStore.Controllers
                 return NotFound();
             }
             return Ok(category);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var createdCategory = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = createCategoryDto.Name
+            };
+            await _categoryService.CreateCategoryAsync(createdCategory);
+            return Ok(createdCategory);
+        }
+        [HttpPut]
+        public async Task<ActionResult<Category>> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(updateCategoryDto.Id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            category.Name = updateCategoryDto.Name;
+            await _categoryService.UpdateCategoryAsync(category);
+            return Ok(category);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            var category = await _categoryService.GetCategoryByIdAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            await _categoryService.DeleteCategoryAsync(id);
+            return NoContent();
         }
     }
 }
