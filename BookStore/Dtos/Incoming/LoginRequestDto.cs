f506acdf7eaf383d19d763521aff1660ffed8543
Hasnaa <a02040456@gmail.com>
2026-03-29 20:03:31 +0200

    Login dtos

---
+++ BookStore/Dtos/Incoming/LoginRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Dtos.Incoming
+{
+    public class LoginRequestDto
+    {
+        [Required]
+        [DataType(DataType.Text)]
+        public string UserName { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
