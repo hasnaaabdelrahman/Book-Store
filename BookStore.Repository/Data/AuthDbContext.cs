d5f9b3c531e4e471f55f3e71324495f65be11d43
Hasnaa <a02040456@gmail.com>
2026-03-29 18:55:44 +0200

    Seeding Roles

--- BookStore.Repository/Data/AuthDbContext.cs
+++ BookStore.Repository/Data/AuthDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -13,6 +14,22 @@ namespace BookStore.Repository.Data
         public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
         {
             
+        }
+        override protected void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminRoleId = "9797fe42-d221-49ad-a320-c247ade4d55f";
+            var userRoleId = "68da8133-d7c5-46c4-8745-530ac3ebb613";
+
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole { Id = adminRoleId, ConcurrencyStamp = adminRoleId, Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = userRoleId, ConcurrencyStamp = userRoleId, Name = "User", NormalizedName = "USER" }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
         }
     }
 }
