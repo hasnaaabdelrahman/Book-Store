f46decc428547a16f061f37624b7166650d9da83
Hasnaa <a02040456@gmail.com>
2026-04-13 07:14:43 +0200

    cart services

---
+++ BookStore.Core/Services.Contract/ICartService.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Services.Contract
+{
+    public interface ICartService
+    {
+        public Task AddToCartAsync(Guid userId, Guid bookId, int quantity);
+            public Task RemoveFromCart(Guid userId, Guid bookId);
+            public Task ClearCart(Guid userId);
+            public Task<IEnumerable<CartItem>> GetCartItemsAsync(Guid userId);   
+    }
+}
