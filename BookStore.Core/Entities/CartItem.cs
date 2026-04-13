ea1746edf97db4c3b6b85c05f1578f1058811211
Hasnaa <a02040456@gmail.com>
2026-04-13 07:20:28 +0200

    cart items

--- BookStore.Core/Entities/CartItem.cs
+++ BookStore.Core/Entities/CartItem.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,11 @@ namespace BookStore.Core.Entities
 {
     public class CartItem
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
         public Guid CartId { get; set; }
+        public Cart Cart { get; set; }
         public Guid BookId { get; set; }
         public int Quantity { get; set; }
 
