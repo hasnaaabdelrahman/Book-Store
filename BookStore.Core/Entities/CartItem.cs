0a41ab317635897d260df4666dd076af2a01c353
Hasnaa <a02040456@gmail.com>
2026-04-13 07:14:14 +0200

    add cart item entity

---
+++ BookStore.Core/Entities/CartItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core.Entities
+{
+    public class CartItem
+    {
+        public Guid Id { get; set; }
+        public Guid CartId { get; set; }
+        public Guid BookId { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
